83a18c03ad6dc03173f64cd1c166e4a8a7ec5fcf
Johannes Breitling <j.breitling@suited-technologies.com>
2023-09-01 14:34:49 +0200

    Added sticky navbar

--- js/jbdesign-navbar.js
+++ js/jbdesign-navbar.js
@@ -1,1 +1,20 @@
 console.log("moin")
+
+const nav = document.getElementById("nav");
+
+let stickyBreakpoint = null;
+
+window.addEventListener("scroll", () => {
+
+    if (stickyBreakpoint == null) {
+        stickyBreakpoint = nav.offsetTop;
+    }
+
+    console.log(stickyBreakpoint)
+
+    if (stickyBreakpoint < window.scrollY) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+})
