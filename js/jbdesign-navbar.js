991ce5738fa8cb3274daf86d8162b0debe859a22
Johannes Breitling <j.breitling@suited-technologies.com>
2023-09-07 14:01:38 +0200

    Continued on the mobile nav

--- js/jbdesign-navbar.js
+++ js/jbdesign-navbar.js
@@ -4,6 +4,46 @@ const nav = document.getElementById("nav");
 
 const mobileNavToggler = document.getElementById("mobile-nav-toggler");
 
+/*
+const submenus = document.getElementsByClassName("submenu");
+
+for (let i = 0; i < submenus.length; i++) {
+    console.log(submenus[i])
+
+    console.log(submenus[i].children)
+}
+*/
+
+
+const navItems = document.getElementsByClassName("item-container")
+console.log(navItems)
+
+let findChild = (parent, childClass) => {
+    for (let child of parent.children) {
+        if (child.classList.contains(childClass)) return child;
+    }
+
+    return null;
+}
+
+for (let navItem of navItems) {
+
+    // Find submenu
+    let submenu = findChild(navItem, "submenu");
+    // Skip the rest if there is no submenu in this child
+    if (submenu == null) {
+        continue;
+    }
+
+    // Find the button to open the submenu
+    let openLink = findChild(navItem, "item");
+
+    console.log(submenu, openLink)
+}
+
+
+
+
 let stickyBreakpoint = null;
 
 window.addEventListener("scroll", () => {
@@ -32,3 +72,4 @@ mobileNavToggler.addEventListener("click", () => {
         nav.classList.remove("close");
     }
 })
+
